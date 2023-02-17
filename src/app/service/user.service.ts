46dbd59eee8251ed51a4325cf0213fdd9a2705af
horn1347 <horn1347@gmail.com>
2023-02-17 12:24:35 +0100

    Introduce user http service

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor(private httpClient: HttpClient) { }
+}
