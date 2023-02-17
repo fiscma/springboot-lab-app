060ce8b05903663d66d599ca99797e91571ae571
horn1347 <horn1347@gmail.com>
2023-02-17 16:07:07 +0100

    Fuck you CORS

--- src/app/service/user-service.service.ts
+++ src/app/service/user-service.service.ts
@@ -2,11 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
+  result: String[] = [];
+
   constructor(private httpClient: HttpClient) { }
+
+  ngOnInit () {
+    this.httpClient.get('http://localhost:8088/users/find/all')
+    .subscribe(data => {
+      this.result = (data as any)['userName'];
+      console.log(this.result);
+    });
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { withInterceptorsFromDi } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { USERS } from '../mock-users';
 import { User } from '../user';
+import { HttpClient } from '@angular/common/http';
+import { UserServiceService } from '../service/user-service.service';
+import { AppComponent } from '../app.component';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
 export class UsersComponent {
+  constructor(private httpClient: HttpClient) { }
+
+  result!: string;
+
+  customHeaders= new HttpHeaders()
+  .set('Access-Control-Allow-Origin', '*');
+
+  ngOnInit () {
+    this.getUsers();
+    console.log(this.customHeaders);
+  }
+
+  getUsers() {
+    this.httpClient.get('http://localhost:8088/api/user/get', {'headers': this.customHeaders})
+    .subscribe(data => {
+      this.result = (data as any)['userName'];
+      console.log(this.result);
+    });
+  }
+
   users = USERS;
 
   selectedUser?: User;
   onSelect(hero: User): void {
   this.selectedUser = hero;
+
 }
 
 }
